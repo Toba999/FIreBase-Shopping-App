0449c637a4b98102ec4a575862949fa48378629c
Toba <mohamedtowba298@gmail.com>
2021-06-24 11:20:48 +0200

    firebaseShoppingApp

---
+++ app/src/main/java/com/example/android/oshoppingapp/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,27 @@
+package com.example.android.oshoppingapp.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.oshoppingapp.R
+
+class DashboardFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+            textView.text = "this is the dashboard fragment"
+
+        return root
+    }
+}
---
+++ app/src/main/java/com/example/android/oshoppingapp/ui/home/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.example.android.oshoppingapp.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.oshoppingapp.R
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+            textView.text = "this is home fragment"
+
+        return root
+    }
+}
