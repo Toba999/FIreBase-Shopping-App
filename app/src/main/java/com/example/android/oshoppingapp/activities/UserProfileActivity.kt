e61cebaf1d5f395f9ff15877cb9f8d53b22cf02e
Toba <mohamedtowba298@gmail.com>
2021-06-30 16:59:07 +0200

    firebaseShoppingApp

--- app/src/main/java/com/example/android/oshoppingapp/activities/UserProfileActivity.kt
+++ app/src/main/java/com/example/android/oshoppingapp/activities/UserProfileActivity.kt
@@ -14,8 +14,10 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.android.oshoppingapp.R
 import com.example.android.oshoppingapp.databinding.ActivityUserProfileBinding
+import com.example.android.oshoppingapp.firestore.FireStoreClass
 import com.example.android.oshoppingapp.models.User
 import com.example.android.oshoppingapp.utils.Constants
+import com.example.android.oshoppingapp.utils.GlideLoader
 import java.io.IOException
 
 class UserProfileActivity : BaseActivity(), View.OnClickListener {
@@ -23,11 +25,11 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
     private var mSelectedImageFileUri: Uri? = null
     private var mUserProfileImageURL: String = ""
 
-    private lateinit var binding:ActivityUserProfileBinding
+    private lateinit var binding: ActivityUserProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityUserProfileBinding.inflate()
+        binding = ActivityUserProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         if (intent.hasExtra(Constants.EXTRA_USER_DETAILS)) {
@@ -38,49 +40,49 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
         // If the profile is incomplete then user is from login screen and wants to complete the profile.
         if (mUserDetails.profileCompleted == 0) {
             // Update the title of the screen to complete profile.
-            tv_title.text = resources.getString(R.string.title_complete_profile)
+            binding.tvTitle.text = resources.getString(R.string.title_complete_profile)
 
             // Here, the some of the edittext components are disabled because it is added at a time of Registration.
-            et_first_name.isEnabled = false
-            et_first_name.setText(mUserDetails.firstName)
+            binding.etFirstNAme.isEnabled = false
+            binding.etFirstNAme.setText(mUserDetails.firstName)
 
-            et_last_name.isEnabled = false
-            et_last_name.setText(mUserDetails.lastName)
+            binding.etLastName.isEnabled = false
+            binding.etLastName.setText(mUserDetails.lastName)
 
-            et_email.isEnabled = false
-            et_email.setText(mUserDetails.email)
+            binding.etEmail.isEnabled = false
+            binding.etEmail.setText(mUserDetails.email)
         } else {
 
             // Call the setup action bar function.
             setupActionBar()
 
             // Update the title of the screen to edit profile.
-            tv_title.text = resources.getString(R.string.title_edit_profile)
+            binding.tvTitle.text = resources.getString(R.string.title_edit_profile)
 
             // Load the image using the GlideLoader class with the use of Glide Library.
-            GlideLoader(this@UserProfileActivity).loadUserPicture(mUserDetails.image, iv_user_photo)
+            GlideLoader(this@UserProfileActivity).loadUserPicture(mUserDetails.image, binding.ivUserPhoto)
 
             // Set the existing values to the UI and allow user to edit except the Email ID.
-            et_first_name.setText(mUserDetails.firstName)
-            et_last_name.setText(mUserDetails.lastName)
+            binding.etFirstNAme.setText(mUserDetails.firstName)
+            binding.etLastName.setText(mUserDetails.lastName)
 
-            et_email.isEnabled = false
-            et_email.setText(mUserDetails.email)
+            binding.etEmail.isEnabled = false
+            binding.etEmail.setText(mUserDetails.email)
 
             if (mUserDetails.mobile != 0L) {
-                et_mobile_number.setText(mUserDetails.mobile.toString())
+                binding.etMobileNumber.setText(mUserDetails.mobile.toString())
             }
             if (mUserDetails.gender == Constants.MALE) {
-                rb_male.isChecked = true
+                binding.rbMale.isChecked = true
             } else {
-                rb_female.isChecked = true
+                binding.rbFemale.isChecked = true
             }
         }
 
         // Assign the on click event to the user profile photo.
-        iv_user_photo.setOnClickListener(this@UserProfileActivity)
+        binding.ivUserPhoto.setOnClickListener(this@UserProfileActivity)
         // Assign the on click event to the SAVE button.
-        btn_save.setOnClickListener(this@UserProfileActivity)
+        binding.btnSubmit.setOnClickListener(this@UserProfileActivity)
     }
 
     override fun onClick(v: View?) {
@@ -88,18 +90,13 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
             when (v.id) {
 
                 R.id.iv_user_photo -> {
-
                     if (ContextCompat.checkSelfPermission(
-                                    this,
-                                    Manifest.permission.READ_EXTERNAL_STORAGE
-                            )
+                                    this, Manifest.permission.READ_EXTERNAL_STORAGE)
                             == PackageManager.PERMISSION_GRANTED
                     ) {
                         Constants.showImageChooser(this@UserProfileActivity)
                     } else {
-                        /*Requests permissions to be granted to this application. These permissions
-                         must be requested in your manifest, they should not be granted to your app,
-                         and they should have protection level*/
+
                         ActivityCompat.requestPermissions(
                                 this,
                                 arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
@@ -107,23 +104,15 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
                         )
                     }
                 }
-
-                R.id.btn_save -> {
-
+                R.id.btnSubmit -> {
                     if (validateUserProfileDetails()) {
-
                         // Show the progress dialog.
                         showProgressDialog(resources.getString(R.string.please_wait))
-
                         if (mSelectedImageFileUri != null) {
-
-                            FirestoreClass().uploadImageToCloudStorage(
+                            FireStoreClass().uploadImageToCloudStorage(
                                     this@UserProfileActivity,
-                                    mSelectedImageFileUri,
-                                    Constants.USER_PROFILE_IMAGE
-                            )
+                                    mSelectedImageFileUri, Constants.USER_PROFILE_IMAGE)
                         } else {
-
                             updateUserProfileDetails()
                         }
                     }
@@ -132,13 +121,6 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
         }
     }
 
-    /**
-     * This function will identify the result of runtime permission after the user allows or deny permission based on the unique code.
-     *
-     * @param requestCode
-     * @param permissions
-     * @param grantResults
-     */
     override fun onRequestPermissionsResult(
             requestCode: Int,
             permissions: Array<out String>,
@@ -160,42 +142,23 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
         }
     }
 
-    /**
-     * Receive the result from a previous call to
-     * {@link #startActivityForResult(Intent, int)}.  This follows the
-     * related Activity API as described there in
-     * {@link Activity#onActivityResult(int, int, Intent)}.
-     *
-     * @param requestCode The integer request code originally supplied to
-     *                    startActivityForResult(), allowing you to identify who this
-     *                    result came from.
-     * @param resultCode The integer result code returned by the child activity
-     *                   through its setResult().
-     * @param data An Intent, which can return result data to the caller
-     *               (various data can be attached to Intent "extras").
-     */
     public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == Constants.PICK_IMAGE_REQUEST_CODE) {
                 if (data != null) {
                     try {
-
                         // The uri of selected image from phone storage.
                         mSelectedImageFileUri = data.data!!
-
                         GlideLoader(this@UserProfileActivity).loadUserPicture(
                                 mSelectedImageFileUri!!,
-                                iv_user_photo
-                        )
+                                binding.ivUserPhoto)
                     } catch (e: IOException) {
                         e.printStackTrace()
                         Toast.makeText(
                                 this@UserProfileActivity,
                                 resources.getString(R.string.image_selection_failed),
-                                Toast.LENGTH_SHORT
-                        )
-                                .show()
+                                Toast.LENGTH_SHORT).show()
                     }
                 }
             }
@@ -205,12 +168,9 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
         }
     }
 
-    /**
-     * A function for actionBar Setup.
-     */
     private fun setupActionBar() {
 
-        setSupportActionBar(toolbar_user_profile_activity)
+        setSupportActionBar(binding.toolbarUserProfileActivity)
 
         val actionBar = supportActionBar
         if (actionBar != null) {
@@ -218,21 +178,13 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
         }
 
-        toolbar_user_profile_activity.setNavigationOnClickListener { onBackPressed() }
+        binding.toolbarUserProfileActivity.setNavigationOnClickListener { onBackPressed() }
     }
 
-    /**
-     * A function to validate the input entries for profile details.
-     */
+
     private fun validateUserProfileDetails(): Boolean {
         return when {
-
-            // We have kept the user profile picture is optional.
-            // The FirstName, LastName, and Email Id are not editable when they come from the login screen.
-            // The Radio button for Gender always has the default selected value.
-
-            // Check if the mobile number is not empty as it is mandatory to enter.
-            TextUtils.isEmpty(et_mobile_number.text.toString().trim { it <= ' ' }) -> {
+            TextUtils.isEmpty(binding.etMobileNumber.text.toString().trim { it <= ' ' }) -> {
                 showErrorSnackBar(resources.getString(R.string.err_msg_enter_mobile_number), true)
                 false
             }
@@ -242,28 +194,25 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
         }
     }
 
-    /**
-     * A function to update user profile details to the firestore.
-     */
     private fun updateUserProfileDetails() {
 
         val userHashMap = HashMap<String, Any>()
 
         // Get the FirstName from editText and trim the space
-        val firstName = et_first_name.text.toString().trim { it <= ' ' }
+        val firstName = binding.etFirstNAme.text.toString().trim { it <= ' ' }
         if (firstName != mUserDetails.firstName) {
             userHashMap[Constants.FIRST_NAME] = firstName
         }
 
         // Get the LastName from editText and trim the space
-        val lastName = et_last_name.text.toString().trim { it <= ' ' }
+        val lastName = binding.etLastName.text.toString().trim { it <= ' ' }
         if (lastName != mUserDetails.lastName) {
             userHashMap[Constants.LAST_NAME] = lastName
         }
 
         // Here we get the text from editText and trim the space
-        val mobileNumber = et_mobile_number.text.toString().trim { it <= ' ' }
-        val gender = if (rb_male.isChecked) {
+        val mobileNumber = binding.etMobileNumber.text.toString().trim { it <= ' ' }
+        val gender = if (binding.rbMale.isChecked) {
             Constants.MALE
         } else {
             Constants.FEMALE
@@ -281,23 +230,17 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
             userHashMap[Constants.GENDER] = gender
         }
 
-        // Here if user is about to complete the profile then update the field or else no need.
-        // 0: User profile is incomplete.
-        // 1: User profile is completed.
         if (mUserDetails.profileCompleted == 0) {
             userHashMap[Constants.COMPLETE_PROFILE] = 1
         }
-
         // call the registerUser function of FireStore class to make an entry in the database.
-        FirestoreClass().updateUserProfileData(
+        FireStoreClass().updateUserProfileData(
                 this@UserProfileActivity,
                 userHashMap
         )
     }
 
-    /**
-     * A function to notify the success result and proceed further accordingly after updating the user details.
-     */
+
     fun userProfileUpdateSuccess() {
 
         // Hide the progress dialog
@@ -315,14 +258,11 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
         finish()
     }
 
-    /**
-     * A function to notify the success result of image upload to the Cloud Storage.
-     *
-     * @param imageURL After successful upload the Firebase Cloud returns the URL.
-     */
+
     fun imageUploadSuccess(imageURL: String) {
 
         mUserProfileImageURL = imageURL
 
         updateUserProfileDetails()
     }
+}
